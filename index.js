0393497922a82d1540ff68d63aeb774a375f4e1e
Bolaji Olajide <cooproton@gmail.com>
2020-11-22 00:32:02 +0100

    feat: handle errors better

--- index.js
+++ index.js
@@ -11,30 +11,37 @@ const app = express();
 app.get('/', (_, res) => res.json('It works!'));
 
 app.get('/search', async (req, res) => {
-  const mongoClient = await fetchMongoClient();
+  try {
+    const mongoClient = await fetchMongoClient();
 
-  const { q } = req.query;
+    const { q } = req.query;
 
-  if (q) {
-    const topic = await mongoClient.collection('topics').findOne({ name: q });
-    const _subtopics =  await mongoClient
-      .collection('topics')
-      .find({ left: { $gt: topic.left }, right: { $lt: topic.right } });
+    if (q) {
+      const topic = await mongoClient.collection('topics').findOne({ name: q });
+      const _subtopics = await mongoClient
+        .collection('topics')
+        .find({ left: { $gt: topic.left }, right: { $lt: topic.right } });
 
-    const subtopics = (await _subtopics.toArray()).map(subtopic => subtopic.name);
+      const subtopics = (await _subtopics.toArray()).map(subtopic => subtopic.name);
 
-    const questions = await mongoClient
-      .collection('questions')
-      .find({ annotations: { $in: subtopics } })
-      .toArray();
+      const questions = await mongoClient
+        .collection('questions')
+        .find({ annotations: { $in: subtopics } })
+        .toArray();
 
-    return res.json({ status: 'success', questions });
+      return res.json({ status: 'success', questions });
+    }
+
+    return res.status(400).json({
+      status: 'error',
+      message: 'Query string wasn\'t provided'
+    });
+  } catch (error) {
+    return res.status(422).json({
+      status: 'error',
+      message: `There was an error searching for the topic. ${error.message}`
+    });
   }
-
-  return res.status(400).json({
-    status: 'error',
-    message: 'Query string wasn\'t provided'
-  });
 });
 
 if (config.isDev) {
